3203487651593656c9ee97b97c085c3acb09ff79
Edward O'Dell <ed@epocreative.com>
2016-06-29 20:13:07 -0400

    Created 063016 homework files

---
+++ cheat.js
@@ -0,0 +1,39 @@
+
+
+console.log("hello world");
+
+alert("hello world");
+
+function logTOHTML() {
+	document.getElementByID("title".innerHTML = "Simple Title")
+}
+
+var x = 20;
+var y = 30;
+console.log(x + y);
+
+var colors = ["gold", "silver", "bronze"];
+
+var size = [10, 20, 50, 100];
+
+var colorsAndSize = [colors, size];
+
+console.log(colorsAndSize[0][2]);
+console.log(colorsAndSize[1][3]);
+
+// These is what defines me
+
+var epo = {
+  gender: "male",
+  sign: "aries",
+  shoesize: 10,
+  interests: ["cycling", "vizslas", 7]
+}
+
+console.log(epo.gender);
+console.log(epo.interests[0]);
+
+var first="Ed "
+var last="O'Dell"
+
+console.log(first+last);
